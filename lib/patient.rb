7f7cdf55331a1b7d86730f784ae890e943c98f24
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-07-08 01:00:02 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,29 @@
 class Patient 
 
-attr_accessor :
+attr_accessor :name
 @@all = []
 
-def initialize 
+def initialize(name)
+@name = name 
+@@all << self 
+end 
+
+def self.all 
+  @@all 
+end 
+
+def new_appointment(date,doctor)
+
+end 
+
+def appointments
+  
+  #iterates through appts/returns ones with relation to self 
+end 
+
+def doctors
+  
+  #iterates over appts/collects doctor w appt relation
+end 
 
 end #class
