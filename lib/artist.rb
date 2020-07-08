c068fc368d923f5a0226a80abab11074a97968db
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-07-08 00:37:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,22 +14,18 @@ end
 
 def new_song(name,genre) 
   Song.new(name,self,genre)
-  #creates new song w/relationship to artist
 end 
 
 def songs
   Song.all.select do |song|
     song.artist == self 
-#iterate through "Songs", finds ones with relationship to artist #"select"
 end 
-end 
+ end 
 
 def genres 
     self.songs.collect do |song|
       song.genre
-#same as #songs, but with genres 
 end 
-end  
+ end  
 
-
- end  #class end 
+end 
