3d421daedda3022cf50684ef8da499ae7ead2a7f
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-07-08 01:32:08 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -24,8 +24,8 @@ end
 end 
 
 def doctors
-  self.appointments.collect do |dr|
-    dr.patient == self
+  self.appointments.collect do |practicitioner|
+    .patient == self
   #iterates over appts/collects doctor w appt relation
 end 
 end 
